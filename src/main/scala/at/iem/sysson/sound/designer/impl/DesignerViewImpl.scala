682f3787a3992133cb80d12c06bea519ac715ba1
Sciss <contact@sciss.de>
2013-02-04 15:57:43 +0100

    ...

--- src/main/scala/at/iem/sysson/sound/designer/impl/DesignerViewImpl.scala
+++ src/main/scala/at/iem/sysson/sound/designer/impl/DesignerViewImpl.scala
@@ -7,19 +7,20 @@ import collection.immutable.{IndexedSeq => IIdxSeq}
 import collection.breakOut
 import prefuse.{Visualization, Display}
 import swing.{Action, Component, Frame}
-import javax.swing.JComponent
+import javax.swing.{JTextField, JComponent}
 import gui.GUI
-import java.awt.event.{MouseEvent, MouseMotionAdapter, KeyEvent}
+import java.awt.event.{ActionEvent, ActionListener, MouseEvent, MouseMotionAdapter, KeyEvent}
 import prefuse.util.display.PaintListener
-import java.awt.{BasicStroke, Color, Shape, Graphics2D}
+import java.awt.{GraphicsEnvironment, Font, Rectangle, BasicStroke, Color, Shape, Graphics2D}
 import prefuse.data.Graph
-import prefuse.controls.{ZoomingPanControl, PanControl, DragControl, ControlAdapter}
-import javax.swing.event.MouseInputAdapter
+import prefuse.controls.{ZoomControl, ZoomingPanControl, PanControl, DragControl, ControlAdapter}
+import javax.swing.event.{DocumentEvent, DocumentListener, MouseInputAdapter}
 import java.awt.geom.{Rectangle2D, Point2D}
 import prefuse.action.{RepaintAction, ActionList}
-import prefuse.render.{AbstractShapeRenderer, LabelRenderer, DefaultRendererFactory}
+import prefuse.render.{Renderer, AbstractShapeRenderer, LabelRenderer, DefaultRendererFactory}
 import prefuse.util.ColorLib
 import prefuse.visual.VisualItem
+import javax.swing.text.JTextComponent
 
 object DesignerViewImpl {
   def apply(): DesignerView = new Impl
@@ -45,15 +46,15 @@ object DesignerViewImpl {
   final case class Constant(values: IIdxSeq[Float]) extends Input
 
   object ElementState {
-    case object New extends ElementState
+    case object Edit  extends ElementState
     case object Error extends ElementState
-    case object Ok extends ElementState
+    case object Ok    extends ElementState
   }
   sealed trait ElementState
 
   sealed trait VisualElement {
     var name: String = ""
-    var state: ElementState = ElementState.New
+    var state: ElementState = ElementState.Edit
   }
   final class VisualGraphElem extends VisualElement {
     var content = Option.empty[GraphElem]
@@ -92,49 +93,92 @@ object DesignerViewImpl {
 
   private final val ACTION_REPAINT  = "repaint"
 
-  private object BoxRenderer extends AbstractShapeRenderer {
-    private val r = new Rectangle2D.Float()
+//  private object TextEditor extends JTextField() {
+//    setBorder(null)
+//    setVisible(false)
+//    addActionListener(new ActionListener {
+//      def actionPerformed(e: ActionEvent) {
+//        println("JUHU " + getText)
+//      }
+//    })
+//  }
 
-    val strkColrOk  = ColorLib.getColor(192, 192, 192)
-    val strkColrNew = ColorLib.getColor(  0,   0, 240)
-    val strkColrErr = ColorLib.getColor(240,   0,   0)
-    val fillColr    = ColorLib.getColor(246, 248, 248)
-    val textColr    = Color.black
-    val strkShpOk   = new BasicStroke(1f)
-    val strkShpPend = new BasicStroke(1f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10f, Array[Float](6, 4), 0f)
-
-    protected def getRawShape(vi: VisualItem): Shape = {
-      var x    = vi.getX
-      if (x.isNaN || x.isInfinity) x = 0.0
-      var y    = vi.getY
-      if (y.isNaN || y.isInfinity) y = 0.0
-      r.setRect(x, y, MIN_BOX_WIDTH, BOX_HEIGHT)
-      r
+  private val font = {
+    val fntNames  = GraphicsEnvironment.getLocalGraphicsEnvironment.getAvailableFontFamilyNames
+    val fntMenlo  = "Menlo"
+    val name      = if (fntNames.contains(fntMenlo)) {
+      fntMenlo
+    } else {
+      Font.MONOSPACED
     }
-
-    override def render(g: Graphics2D, vi: VisualItem) {
-      val data = vi.get(COL_ELEM).asInstanceOf[VisualElement]
-      if (data == null) return
-      val r = getShape(vi)
-      g.setColor(fillColr)
-      g.fill(r)
-      data.state match {
-        case ElementState.Ok =>
-          g.setColor(strkColrOk)
-          g.setStroke(strkShpOk)
-        case ElementState.New =>
-          g.setColor(strkColrNew)
-          g.setStroke(strkShpPend)
-        case ElementState.Error =>
-          g.setColor(strkColrErr)
-          g.setStroke(strkShpPend)
-      }
-      g.draw(r)
-     }
+    new Font(name, Font.PLAIN, 11)
   }
 
   private final class Impl extends DesignerView {
 //    private var mode: Mode = Mode.Select
+
+    var editingNode     = Option.empty[VisualItem]
+    var editingOldText  = ""
+
+    private object BoxRenderer extends AbstractShapeRenderer {
+      private val r = new Rectangle2D.Float()
+
+      val strkColrOk  = ColorLib.getColor(192, 192, 192)
+      val strkColrEdit= ColorLib.getColor(  0,   0, 240)
+      val strkColrErr = ColorLib.getColor(240,   0,   0)
+      val fillColr    = ColorLib.getColor(246, 248, 248)
+      val textColrEdit= strkColrEdit
+      val textColr    = Color.black
+      val strkShpOk   = new BasicStroke(1f)
+      val strkShpPend = new BasicStroke(1f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10f, Array[Float](6, 4), 0f)
+
+      protected def getRawShape(vi: VisualItem): Shape = {
+        var x    = vi.getX
+        if (x.isNaN || x.isInfinity) x = 0.0
+        var y    = vi.getY
+        if (y.isNaN || y.isInfinity) y = 0.0
+
+        val w = getData(vi) match {
+          case Some(data) =>
+            val fm    = Renderer.DEFAULT_GRAPHICS.getFontMetrics(font)
+            math.max(MIN_BOX_WIDTH, fm.stringWidth(data.name) + 6)
+          case _ => MIN_BOX_WIDTH
+        }
+
+        r.setRect(x, y, w, BOX_HEIGHT)
+        r
+      }
+
+      override def render(g: Graphics2D, vi: VisualItem) {
+        getData(vi).foreach { data =>
+          val r = getShape(vi)
+          val b = r.getBounds2D
+          g.setColor(fillColr)
+          g.fill(r)
+          data.state match {
+            case ElementState.Ok =>
+              g.setColor(strkColrOk)
+              g.setStroke(strkShpOk)
+            case ElementState.Edit =>
+              g.setColor(strkColrEdit)
+              g.setStroke(strkShpPend)
+            case ElementState.Error =>
+              g.setColor(strkColrErr)
+              g.setStroke(strkShpPend)
+          }
+          g.draw(r)
+          data.state match {
+            case ElementState.Edit =>
+              g.setColor(textColrEdit)
+            case _ =>
+              g.setColor(textColr)
+          }
+          g.setFont(font)
+          val fm = Renderer.DEFAULT_GRAPHICS.getFontMetrics(font)
+          g.drawString(data.name, b.getX.toFloat + 3, b.getY.toFloat + 2 + fm.getAscent)
+        }
+      }
+    }
 
     val vis   = new Visualization
     val disp  = {
@@ -170,8 +214,8 @@ object DesignerViewImpl {
     private val mousePoint = new Point2D.Float()
 
     def reportMouse(e: MouseEvent) {
-      val t     = disp.getTransform
-      t.transform(e.getPoint, mousePoint)
+      val at = disp.getInverseTransform
+      at.transform(e.getPoint, mousePoint)
 //println(s"mouse screen ${e.getPoint} - virt ${mousePoint}")
     }
 
@@ -193,7 +237,53 @@ object DesignerViewImpl {
     disp.addMouseListener(mouseCoordListener)
     disp.addMouseMotionListener(mouseCoordListener)
     disp.addControlListener(new DragControl())
-    disp.addControlListener(new ZoomingPanControl())
+//    disp.addControlListener(new ZoomingPanControl())
+    disp.addControlListener(new PanControl())
+    disp.addControlListener(new ZoomControl())
+//    disp.setTextEditor(TextEditor)
+
+    def getData(vi: VisualItem): Option[VisualElement] = Option(vi.get(COL_ELEM).asInstanceOf[VisualElement])
+
+    disp.getTextEditor match {
+      case tf: JTextField =>
+        tf.setFont(font)
+        tf.setForeground(BoxRenderer.strkColrEdit)
+        tf.setBackground(BoxRenderer.fillColr)
+        tf.getDocument.addDocumentListener(new DocumentListener {
+          def refreshBox() {
+            editingNode.foreach { vi =>
+              getData(vi).foreach { data =>
+                data.name = tf.getText
+//                vi.set(COL_ELEM, data)
+//                vis.repaint()
+                val r = updateEditingBounds(vi)
+//                println("BOUNDS " + r)
+                tf.setSize(r.getSize)
+                vis.repaint()
+              }
+            }
+          }
+
+          def insertUpdate( e: DocumentEvent) { refreshBox() }
+          def removeUpdate( e: DocumentEvent) { refreshBox() }
+          def changedUpdate(e: DocumentEvent) { refreshBox() }
+        })
+        tf.addActionListener(new ActionListener {
+          def actionPerformed(e: ActionEvent) {
+            disp.stopEditing()
+            editingNode.foreach { vi =>
+              editingNode = None
+              getData(vi).foreach { data =>
+                data.name   = tf.getText
+                if (data.name != editingOldText) {
+                  data.state  = if (Collection.map.contains(data.name)) ElementState.Ok else ElementState.Error
+                  vis.repaint()
+                }
+              }
+            }
+          }
+      })
+    }
 
     val rf = new DefaultRendererFactory(BoxRenderer)
     vis.setRendererFactory(rf)
@@ -208,15 +298,35 @@ object DesignerViewImpl {
 
 //    vis.putAction(ACTION_REPAINT, new RepaintAction())
 
+    def updateEditingBounds(vi: VisualItem): Rectangle = {
+//      vi.validateBounds()
+      vi.setValidated(false)  // this causes a subsequent call to getBounds to ask the renderer again, plus creates dirty screen region
+      val b      = vi.getBounds
+      val at     = disp.getTransform
+      val r      = at.createTransformedShape(b).getBounds
+      r.x       += 3
+      r.y       += 1
+      r.width   -= 5
+      r.height  -= 2
+      r
+    }
+
+    def editObject(vi: VisualItem) {
+      getData(vi).foreach { data =>
+        val r = updateEditingBounds(vi)
+        editingNode     = Some(vi)
+        editingOldText  = data.name
+        disp.editText("", r)
+      }
+    }
+
     def enterPutObject() {
       val n   = g.addNode()
       n.set(COL_ELEM, new VisualGraphElem)
       val vi  = vis.getVisualItem(GROUP_GRAPH, n)
-//      vi.setBounds(mousePoint.getX, mousePoint.getY, MIN_BOX_WIDTH, BOX_HEIGHT)
       vi.setX(mousePoint.getX - 2)
       vi.setY(mousePoint.getY - 2)
-//      vi.setFont()
-//      vi.setStrokeColor(ColorLib.gray(0))
+      editObject(vi)
       vis.repaint()
     }
   }
