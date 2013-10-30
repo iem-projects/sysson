7a309e00dd6e5972a3359f8a13fe0485bb40d70b
Sciss <contact@sciss.de>
2013-10-30 12:28:36 +0100

    actually... do we need LeafLike[A] at all?

--- src/main/scala/at/iem/sysson/Library.scala
+++ src/main/scala/at/iem/sysson/Library.scala
@@ -13,9 +13,9 @@ object Library {
   }
 
   trait Branch[S <: Sys[S]] extends TreeLike.BranchLike[S, Element, Branch[S], Leaf[S]] with NodeLike[S]
-
-  trait Leaf[S <: Sys[S]] extends TreeLike.LeafLike[Element] with NodeLike[S]
+  trait Leaf  [S <: Sys[S]] extends TreeLike.LeafLike[Element] with NodeLike[S]
 }
 trait Library[S <: Sys[S]] extends TreeLike[S, Library.Element, Unit, Unit, Library[S]] {
-
+  type Leaf   = Library.Leaf  [S]
+  type Branch = Library.Branch[S]
 }
