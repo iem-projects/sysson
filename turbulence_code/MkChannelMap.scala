1736ed1c3dddb8a0f275ccb04542672e3e9a4a89
Sciss <contact@sciss.de>
2014-09-09 19:20:26 +0200

    ...

---
+++ turbulence_code/MkChannelMap.scala
@@ -0,0 +1,12 @@
+import at.iem.sysson.turbulence._
+val m = Turbulence.ChannelMap
+val f = userHome / "sysson" / "buffers" / "channel-map.aif"
+val af = io.AudioFile.openWrite(f, io.AudioFileSpec(
+  numChannels = 1, numFrames = 14 * 5, sampleRate = 44100)
+)
+val b = Array.tabulate(14)(x => Array.tabulate(5)(y =>
+  m.getOrElse((x, y), 0).toFloat
+)).flatten
+af.write(Array(b))
+af.close()
+
