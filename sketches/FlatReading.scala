21bce798898df766472f053c22ac8a33da6b43b5
Hanns Holger Rutz <contact@sciss.de>
2017-01-25 15:37:58 +0100

    thinking

--- sketches/FlatReading.scala
+++ sketches/FlatReading.scala
@@ -11,21 +11,36 @@ class Variable[A](data: Vec[A], val shape: Vec[Int]) {
     val sz  = if (sections.isEmpty) 0 else (1 /: sections)(_ * _.size)
     val zip = (shape zip sections).reverse
     Vec.tabulate(sz) { i =>
-      val (j, _) = ((0, 1) /: zip) { case ((res, m), (dim, r)) =>
-        val add = r((i / m) % dim) * m
-        (res + add, m * dim)
+      val (j, _, _) = ((0, 1, 1) /: zip) { case ((res, m, n), (dim, r)) =>
+        val add = r((i / n) % r.size) * m
+        (res + add, m * dim, n * r.size)
       }
       data(j)
     }
   }
 }
 
-val v = new Variable[Int](Vec(1 to 24: _*), Vec(2, 3, 4))
-
+val v = new Variable[Int](Vec(1 to 512: _*), Vec(8, 8, 8))
+    
 assert(v.read(Vec(0 until 0, 0 until 0, 0 until 0)) == Vec())
 assert(v.read(Vec(0 to 0, 0 to 0, 0 to 2)) == Vec(1, 2, 3))
-assert(v.read(Vec(1 to 1, 1 to 1, 0 to 2)) == Vec(17, 18, 19))
-assert(v.read(Vec(1 to 1, 2 to 2, 3 to 3)) == Vec(24))
+assert(v.read(Vec(1 to 1, 1 to 1, 0 to 2)) == Vec(73, 74, 75))
+
+val selSome = Vec(
+  2 until 4,  // size 2
+  3 until 6,  // size 3
+  4 until 8   // size 4
+)
+
+assert(v.read(selSome) == Vec(
+  157, 158, 159, 160, 
+  165, 166, 167, 168, 
+  173, 174, 175, 176, 
+  
+  221, 222, 223, 224, 
+  229, 230, 231, 232, 
+  237, 238, 239, 240
+))
 
 trait ChunkReader[A] {
   def read(chunkSize: Int): Vec[A]
@@ -60,7 +75,7 @@ val selSome = Vec(
 calcInSection( 0, selSome)  // [0 0 0]
 calcInSection( 4, selSome)  // [0 1 0]
 
-calcInSection( 5, selSome)  // [0 0 1]
+calcInSection( 5, selSome)  // [0 1 1]
 calcInSection( 9, selSome)  // [0 2 1]
 
 // this is the most interesting case as the highest dimension index changes
@@ -76,3 +91,25 @@ calcInSection(19, selSome)  // [1 1 3]
 calcInSection(20, selSome)  // [1 2 0]
 calcInSection(23, selSome)  // [1 2 3]
 
+/*---
+
+Algorithm:
+
+- s0 := calculate the start indices for current `pos`, as done above through `calcInSection`
+- i0 := in s0, find the right-most dim index where the index (elem in s0) is > 0; if there is none, i0 := 0
+- numProc := min((sizes(i0) - s0(i0)) * mods(i0), chunkRemain)
+- ...
+
+let's see this attempt in the above example (reading 4 times 5 elements, then 4):
+
+*/
+
+(pos = 0, chunk = 5) --> s0 := [0 0 0]; i0 := 0; numProc = min(2 - 0) * 12 = 24, 5) = 5
+
+// that doesn't help; we should also look at s1 (stop indices)
+
+val expected = v.read(selSome)
+assert(expected.size == expected.distinct.size)
+
+// e.g. (2, 3, 4) --> 4 + 3 * 8 + 2 * 64   + 1 = 157 OK
+// e.g. (3, 5, 7) --> 7 + 5 * 8 + 3 * 64   + 1 = 240 OK
