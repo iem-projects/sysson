2e666100baabe6e773768ee4469fec3a41150ff4
Hanns Holger Rutz <contact@sciss.de>
2017-01-25 14:54:54 +0100

    understanding flat 1d read

---
+++ sketches/FlatReading.scala
@@ -0,0 +1,78 @@
+type Vec[+A] = collection.immutable.Vector[A]
+val  Vec     = collection.immutable.Vector
+
+class Variable[A](data: Vec[A], val shape: Vec[Int]) {
+  require(data.size == shape.product)
+  
+  def read(sections: Vec[Range]): Vec[A] = {
+    require(sections.size == shape.size)
+    require(sections.zipWithIndex.forall { case (r, ri) => r.forall(i => i >= 0 && i < shape(ri)) })
+    
+    val sz  = if (sections.isEmpty) 0 else (1 /: sections)(_ * _.size)
+    val zip = (shape zip sections).reverse
+    Vec.tabulate(sz) { i =>
+      val (j, _) = ((0, 1) /: zip) { case ((res, m), (dim, r)) =>
+        val add = r((i / m) % dim) * m
+        (res + add, m * dim)
+      }
+      data(j)
+    }
+  }
+}
+
+val v = new Variable[Int](Vec(1 to 24: _*), Vec(2, 3, 4))
+
+assert(v.read(Vec(0 until 0, 0 until 0, 0 until 0)) == Vec())
+assert(v.read(Vec(0 to 0, 0 to 0, 0 to 2)) == Vec(1, 2, 3))
+assert(v.read(Vec(1 to 1, 1 to 1, 0 to 2)) == Vec(17, 18, 19))
+assert(v.read(Vec(1 to 1, 2 to 2, 3 to 3)) == Vec(24))
+
+trait ChunkReader[A] {
+  def read(chunkSize: Int): Vec[A]
+}
+
+def test(c: ChunkReader[Int]): Unit =
+  assert((1 to 4).map(c.read(5)) ++ c.read(4) == Vec(1 to 24: _*))
+
+// test(???)
+
+def calcInSection(pos: Int, sections: Vec[Range]): Vec[Int] = {
+  val sizes    = sections.map(_.size)
+  val modsDivs = sizes zip sizes.scanRight(1)(_ * _).tail
+  modsDivs.map { case (mod, div) =>
+    (pos / div) % mod
+  }
+}
+
+val selAll = v.shape.map(0 until _)
+calcInSection(1, selAll)
+
+(0 until 24).map(i => calcInSection(i, selAll)).foreach(println)
+
+//////////////////////////////////////////////
+
+val selSome = Vec(
+  2 until 4,  // size 2
+  3 until 6,  // size 3
+  4 until 8   // size 4
+)
+
+calcInSection( 0, selSome)  // [0 0 0]
+calcInSection( 4, selSome)  // [0 1 0]
+
+calcInSection( 5, selSome)  // [0 0 1]
+calcInSection( 9, selSome)  // [0 2 1]
+
+// this is the most interesting case as the highest dimension index changes
+// ; can we do this with two reads?
+calcInSection(10, selSome)  // [0 2 2]
+calcInSection(14, selSome)  // [1 0 2]
+
+calcInSection(15, selSome)  // [1 0 3]
+calcInSection(19, selSome)  // [1 1 3]
+
+// this is an interesting case because only the lowest dim index changes
+// ; this should be solved with a single read
+calcInSection(20, selSome)  // [1 2 0]
+calcInSection(23, selSome)  // [1 2 3]
+
